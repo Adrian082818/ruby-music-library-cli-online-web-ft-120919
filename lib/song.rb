fab2e9453620b83725a73a7754624dc61af0e9b3
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-19 20:19:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,9 +60,11 @@ class Song
 end 
 
 def self.new_from_filename(filename)
+  
   artist, song, genre = filename.spli(" - ")
  song_artist = Artist.find_or_create_by_name(artist)
- song_genre = genre.gsub(".mp3", "")
+ song_genre = 
+ genre.gsub(".mp3", "")
 end 
   
   
